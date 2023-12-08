caa9cc10ee4321ae84982c4cc8d065345ad3b02d
NitroLine <sergey.and.natasha@yandex.ru>
2023-12-08 18:29:58 +0500

    add deploy script without down time

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+config_path="balancer/nginx.conf"
+sed -i "s/oncall:8080/oncall:8080 down/" "$config_path";
+docker exec -it sre-oncall-course-balancer-1 nginx -s reload;
+docker compose down oncall-web;
+docker compose up oncall-web -d --build;
+sed -i "s/ down//" "$config_path";
+sed -i "s/oncall2:8080/oncall2:8080 down/" "$config_path";
+docker exec -it sre-oncall-course-balancer-1 nginx -s reload;
+docker compose down oncall-web-2;
+docker compose up oncall-web-2 -d --build;
+sed -i "s/ down//" "$config_path";
+docker exec -it sre-oncall-course-balancer-1 nginx -s reload;
